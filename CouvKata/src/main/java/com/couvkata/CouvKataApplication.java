231ef54c484d8c3a90610cf52d2cb2c73d20dffe
emchtiwi <emchtiwi@ITEM-S29175.dhcp.aix.fr.sopra>
2018-07-14 14:12:02 +0200

    firt commit

---
+++ CouvKata/src/main/java/com/couvkata/CouvKataApplication.java
@@ -0,0 +1,12 @@
+package com.couvkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouvKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouvKataApplication.class, args);
+	}
+}
